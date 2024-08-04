d0400de7fe19089150bca60ae5a90523aef08ee8
Khafid224 <abdulhafid1230@gmail.com>
2024-08-04 18:01:45 +0700

    Konfigurasi route login

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 //import Toaster
 import { Toaster } from 'react-hot-toast';
 
+//import routes
+import Routes from './routes/routes';
+
 function App() {
 
   return (
     <>
       <Toaster />
+      <Routes />
     </>
   )
 }
