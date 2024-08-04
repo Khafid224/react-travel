e06a1d11175ef2546aa3545c5a6ed1d1b324a3b7
Khafid224 <abdulhafid1230@gmail.com>
2024-08-04 22:19:38 +0700

    Konfigurasi pivate route dashboard

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -6,7 +6,13 @@ import { Routes, Route } from "react-router-dom";
 //=======================================================================
 
 //import view Login
-import Login from '../pages/admin/login.jsx';
+import Login from '../pages/admin/Login.jsx';
+
+//import component private routes
+import PrivateRoute from "./PrivateRoutes";
+
+//import view admin Dashboard
+import Dashboard from '../pages/admin/dashboard/index.jsx';
 
 function RoutesIndex() {
     return (
@@ -14,6 +20,16 @@ function RoutesIndex() {
 
             {/* route "/admin/login" */}
             <Route path="/admin/login" element={<Login />} />
+
+            {/* private route "/admin/dashboard" */}
+            <Route
+                path="/admin/dashboard"
+                element={
+                        <PrivateRoute>
+                            <Dashboard />
+                        </PrivateRoute>
+                }
+            />
 
         </Routes>
     )
