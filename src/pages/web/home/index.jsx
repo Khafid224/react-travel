ff9dfe82f64823d6f3e84fcc25cf2236f564d81d
Khafid224 <abdulhafid1230@gmail.com>
2024-08-06 22:15:53 +0700

    menampilkan data slider di home

--- src/pages/web/home/index.jsx
+++ src/pages/web/home/index.jsx
@@ -4,10 +4,16 @@ import React from "react";
 //import layout web
 import LayoutWeb from "../../../layouts/web";
 
+//import Slider component
+import Slider from '../../../components/web/slider';
+
 function Home() {
   return (
     <React.Fragment>
       <LayoutWeb>
+      
+        <Slider />
+
         <div className="container mt-5">
             <h1>Home</h1>
         </div>
